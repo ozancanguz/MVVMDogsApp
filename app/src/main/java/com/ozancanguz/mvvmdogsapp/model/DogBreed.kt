854dc2f3d30af7073bb7e05aaba96d9b4a9868db
Ozan <ozancangu24@gmail.com>
2022-09-14 16:21:51 +0300

    -model built

---
+++ app/src/main/java/com/ozancanguz/mvvmdogsapp/model/DogBreed.kt
@@ -0,0 +1,17 @@
+package com.ozancanguz.mvvmdogsapp.model
+
+data class DogBreed (
+
+    val breedId:String?,
+    val dogbreed:String?,
+    val lifesSpan:String?,
+    val breedGroup:String?,
+    val breedFor:String?,
+    val temperament:String?,
+    val imageUrl:String?
+
+
+
+
+
+)
